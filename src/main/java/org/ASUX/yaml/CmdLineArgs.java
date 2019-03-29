2c340cfc6c4e8c3eaa62138390ec374b26734939
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-03-29 15:29:35 -0400

    Using CONSTANTS instead of strings to avoid typos.  cleaned up support for --replace command, as code was as a mess.

--- src/main/java/org/ASUX/yaml/CmdLineArgs.java
+++ src/main/java/org/ASUX/yaml/CmdLineArgs.java
@@ -49,6 +49,14 @@ import org.apache.commons.cli.*;
  */
 public class CmdLineArgs {
 
+    private static final String READCMD = "read";
+    private static final String LISTCMD = "list";
+    private static final String DELETECMD = "delete";
+    private static final char REPLACECMDCHAR = 'c';
+    private static final String REPLACECMD = "replace";
+    private static final String INPUTFILE = "inputfile";
+    private static final String OUTPUTFILE = "outputfile";
+
     public boolean verbose = false;
     public boolean isReadCmd = true;
     public boolean isListCmd = false;
@@ -57,6 +65,7 @@ public class CmdLineArgs {
     public String yamlPathStr = "*";
     public String inputFilePath = "/tmp/i";
     public String outputFilePath = "/tmp/o";
+    public String replaceFilePath = "/tmp/r";
 //    public com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE;
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -73,10 +82,10 @@ public class CmdLineArgs {
 
         //----------------------------------
         OptionGroup grp = new OptionGroup();
-        Option readCmdOpt = new Option("r", "read", false, "output all elements that match");
-        Option listCmdOpt = new Option("l", "list", false, "List YAML-Keys of all elements that match");
-        Option delCmdOpt = new Option("d", "delete", false, "Delete all elements that match");
-        Option replCmdOpt = new Option("c", "change", true, "change/replace all elements that match - with json provided on cmdline");
+        Option readCmdOpt = new Option("r", READCMD, false, "output all elements that match");
+        Option listCmdOpt = new Option("l", LISTCMD, false, "List YAML-Keys of all elements that match");
+        Option delCmdOpt = new Option("d", DELETECMD, false, "Delete all elements that match");
+        Option replCmdOpt = new Option(""+REPLACECMDCHAR, REPLACECMD, true, "change/replace all elements that match with json-string provided on cmdline");
             replCmdOpt.setOptionalArg(false);
             replCmdOpt.setArgs(1);
             replCmdOpt.setArgName("new");
@@ -105,11 +114,11 @@ public class CmdLineArgs {
         opt.setRequired(true);
         options.addOption(opt);
 
-        opt = new Option("i", "inputfile", true, "input file path");
+        opt = new Option("i", INPUTFILE, true, "input file path");
         opt.setRequired(true);
         options.addOption(opt);
 
-        opt = new Option("o", "outputfile", true, "output file");
+        opt = new Option("o", OUTPUTFILE, true, "output file");
         opt.setRequired(true);
         options.addOption(opt);
 
@@ -121,13 +130,14 @@ public class CmdLineArgs {
             cmd = parser.parse(options, args);
 
             this.verbose = cmd.hasOption("verbose");
-            this.isReadCmd = cmd.hasOption("read");
-            this.isListCmd = cmd.hasOption("list");
-            this.isDelCmd = cmd.hasOption("delete");
-            this.isReplaceCmd = cmd.hasOption("change");
+            this.isReadCmd = cmd.hasOption(READCMD);
+            this.isListCmd = cmd.hasOption(LISTCMD);
+            this.isDelCmd = cmd.hasOption(DELETECMD);
+            this.isReplaceCmd = cmd.hasOption(REPLACECMD);
             this.yamlPathStr = cmd.getOptionValue("yamlpath");
-            this.inputFilePath = cmd.getOptionValue("inputfile");
-            this.outputFilePath = cmd.getOptionValue("outputfile");
+            this.inputFilePath = cmd.getOptionValue(INPUTFILE);
+            this.outputFilePath = cmd.getOptionValue(OUTPUTFILE);
+            this.replaceFilePath = cmd.getOptionValue(REPLACECMD);
 
 //            this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE; // default behavior
 //            if ( cmd.hasOption( noQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.NONE;
@@ -136,7 +146,7 @@ public class CmdLineArgs {
             //System.err.println("this.quoteType = "+this.quoteType.toString());
 
         } catch (ParseException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace(System.err);
             formatter.printHelp("java <jarL> com.esotericsoftware.yamlbeans.CmdLineArgs", options);
             System.exit(1);
         }
@@ -147,7 +157,7 @@ public class CmdLineArgs {
      */
     public String toString() {
 //        return "verbose="+verbose+" read="+isReadCmd+" delete="+isDelCmd+" list="+isListCmd+"  change="+isReplaceCmd+" yamlPathStr="+yamlPathStr+" inpfile="+inputFilePath+" outputfile="+outputFilePath+" quoting="+quoteType.toString();
-        return "verbose="+verbose+" read="+isReadCmd+" delete="+isDelCmd+" list="+isListCmd+"  change="+isReplaceCmd+" yamlPathStr="+yamlPathStr+" inpfile="+inputFilePath+" outputfile="+outputFilePath;
+        return "verbose="+verbose+" read="+isReadCmd+" delete="+isDelCmd+" list="+isListCmd+"  change="+isReplaceCmd+" yamlPathStr="+yamlPathStr+" inpfile="+inputFilePath+" outputfile="+outputFilePath+" replaceFile="+replaceFilePath;
     }
     
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
