75e85abce6c6fa89401665e32040e86950961461
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-04-24 20:38:52 -0400

    Big Change.  No more writing to stdout.  Return LinkedList<Object>.   Cmd.Java will write it to STDOUT.   keys are Strings now.  LinkedHashMap<String, Object> replaced plain java.util.Map.

--- src/main/java/org/ASUX/yaml/ReadYamlEntry.java
+++ src/main/java/org/ASUX/yaml/ReadYamlEntry.java
@@ -32,8 +32,9 @@
 
 package org.ASUX.yaml;
 
-import java.util.Map;
+// import java.util.Map;
 import java.util.LinkedList;
+// import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 /** <p>This concrete class is minimalistic because I am re-using code to query/traverse a YAML file.   See it's parent-class {@link org.ASUX.yaml.AbstractYamlEntryProcessor}.</p>
@@ -41,14 +42,15 @@ import java.util.LinkedHashMap;
  *  <p>This class contains implementation for 4 "callbacks" - </p><ol><li> whenever there is partial match - on the way to a complete(a.k.a. end2end match) </li><li> whenever a full match is found </li><li> a match failed (which implies, invariably, to keep searching till end of YAML file - but.. is a useful callback if you are using a "negative" pattern to search for YAML elements) </li><li> done processing entire YAML file</li></ol>
  *  <p>This org.ASUX.yaml GitHub.com project and the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com projects, would
  *  simply NOT be possible without the genius Java library <a href="https://github.com/EsotericSoftware/yamlbeans">"com.esotericsoftware.yamlbeans"</a>.</p>
- *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com project.</p>
+ *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org-ASUX.github.io/wiki">org.ASUX Wiki</a> of the GitHub.com projects.</p>
  * @see org.ASUX.yaml.AbstractYamlEntryProcessor
  */
 public class ReadYamlEntry extends AbstractYamlEntryProcessor {
 
     public static final String CLASSNAME = "org.ASUX.yaml.ReadYamlEntry";
 
-    public int count = 0;
+    private int count;
+    private LinkedList<Object> output;
 
     /** The only Constructor.
      *  @param _verbose Whether you want deluge of debug-output onto System.out
@@ -56,6 +58,13 @@ public class ReadYamlEntry extends AbstractYamlEntryProcessor {
     public ReadYamlEntry(boolean _verbose) {
         super(_verbose);
         this.count = 0;
+        this.output = new LinkedList<>();
+    }
+
+    private ReadYamlEntry() {
+        super(false);
+        this.count = 0;
+        this.output = new LinkedList<>();
     }
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -64,7 +73,7 @@ public class ReadYamlEntry extends AbstractYamlEntryProcessor {
     /** This function will be called when a partial match of a YAML path-expression happens.
      * See details and warnings in @see org.ASUX.yaml.AbstractYamlEntryProcessor#onPartialMatch()
      */
-    protected boolean onPartialMatch(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths) {
+    protected boolean onPartialMatch(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths) {
 
         // Do Nothing for "read YAML-entry command"
         return true;
@@ -74,13 +83,16 @@ public class ReadYamlEntry extends AbstractYamlEntryProcessor {
     /** This function will be called when a full/end2end match of a YAML path-expression happens.
      * See details and warnings in @see org.ASUX.yaml.AbstractYamlEntryProcessor#onEnd2EndMatch()
      */
-    protected boolean onEnd2EndMatch(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths) {
+    protected boolean onEnd2EndMatch(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths) {
 
         this.count ++;
         // System.out.print("onEnd2EndMatch: _end2EndPaths =");
         // end2EndPaths.forEach( s -> System.out.print(s+"\t") );
         // System.out.println("onEnd2EndMatch: _key = ["+ _key +"] _map.get(_key) = ["+ _map.get(_key) +"]");
-        System.out.println( (_map.get(_key) == null) ? "null" : _map.get(_key).toString() );
+        Object o = _map.get(_key);
+        final String s = (o == null) ? "null" : o.toString();
+        if ( this.verbose ) System.out.println( s );
+        this.output.add( o ); // could be a string or a java.util.LinkedHashMap&lt;String, Object&gt;
 
         return true;
     }
@@ -89,7 +101,7 @@ public class ReadYamlEntry extends AbstractYamlEntryProcessor {
     /** This function will be called whenever the YAML path-expression fails to match.
      * See details and warnings in @see org.ASUX.yaml.AbstractYamlEntryProcessor#onMatchFail()
      */
-    protected void onMatchFail(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths) {
+    protected void onMatchFail(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths) {
 
             // Do Nothing for "read YAML-entry command"
     }
@@ -101,9 +113,25 @@ public class ReadYamlEntry extends AbstractYamlEntryProcessor {
      *
      * You can fuck with the contents of any of the parameters passed, to your heart's content.
      */
-    protected void atEndOfInput(final Map _map, final YAMLPath _yamlPath) {
+    protected void atEndOfInput(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath) {
 
         if ( this.verbose ) System.out.println("Total=" + this.count );
+    }
+
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+    /**
+     * @return the count of how many matches happened.  This value is also = this.getOutput.size()
+     */
+    public int getCount() {
+        return this.count;
+    }
+
+    /**
+     * @return the output as an LinkedList of objects (either Strings or java.util.LinkedHashMap&lt;String, Object&gt; objects).  This is because the 'rhs' of an 
+     */
+    public LinkedList<Object> getOutput() {
+        return this.output;
     }
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
