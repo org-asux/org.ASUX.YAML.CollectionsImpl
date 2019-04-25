38f3b58edc06d1895d29e5b4058d62b7ad1993cf
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-04-24 20:33:11 -0400

    MAJOR REVAMP. Supports --delimiter.   No more --yamlPath.  Added MACRO & BATCH commands.

--- src/main/java/org/ASUX/yaml/CmdLineArgs.java
+++ src/main/java/org/ASUX/yaml/CmdLineArgs.java
@@ -49,31 +49,39 @@ import org.apache.commons.cli.*;
  */
 public class CmdLineArgs {
 
+    public static final String CLASSNAME = "org.ASUX.yaml.CmdLineArgs";
+
     private static final String READCMD = "read";
     private static final String LISTCMD = "list";
     private static final String DELETECMD = "delete";
-    private static final String MACROCMD = "macro";
     private static final String REPLACECMD = "replace";
     // private static final char REPLACECMDCHAR = 'c'; // -c === --replace
+    private static final String MACROCMD = "macro";
+    private static final String BATCHCMD = "macro";
 
     private static final String INPUTFILE = "inputfile";
     private static final String OUTPUTFILE = "outputfile";
     private static final String YAMLPATH = "yamlpath";
+    private static final String DELIMITER = "delimiter";
     // private static final String PROPERTIES = "properties";
 
     public boolean verbose = false;
     public boolean isReadCmd = true;
     public boolean isListCmd = false;
     public boolean isDelCmd = false;
-    public boolean isMacroCmd = false;
     public boolean isReplaceCmd = false;
-    public String yamlPathStr = "*";
+    public boolean isMacroCmd = false;
+    public boolean isBatchCmd = false;
+
     public String inputFilePath = "/tmp/i";
     public String outputFilePath = "/tmp/o";
-    public String replaceFilePath = null;       // optional argument
-    public String propertiesFilePath = null;    // optional argument
+    public String replaceFilePath = null;       // optional argument, but required for 'replace' command
+    public String propertiesFilePath = null;    // optional argument, but required for 'macro' command
+    public String batchFilePath = null;    // optional argument, but required for 'batch' command
+    public String yamlRegExpStr = "undefined";
 
-    // public com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE;
+    public String yamlPatternDelimiter = ".";
+    // public static com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE;
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     /** Constructor.
@@ -90,21 +98,36 @@ public class CmdLineArgs {
         //----------------------------------
         OptionGroup grp = new OptionGroup();
         Option readCmdOpt = new Option("r", READCMD, false, "output all elements that match");
-        Option listCmdOpt = new Option("l", LISTCMD, false, "List YAML-Keys of all elements that match");
-        Option delCmdOpt = new Option("d", DELETECMD, false, "Delete all elements that match");
-        Option macroCmdOpt = new Option("m", MACROCMD, false, "run input YAML file thru a MACRO processor searching for ${ASUX::__} and replacing __ with values from Properties file");
+            readCmdOpt.setOptionalArg(false);
+            readCmdOpt.setArgs(1);
+            readCmdOpt.setArgName("YAMLPattern");
+        Option listCmdOpt = new Option("l", LISTCMD, true, "List YAML-Keys of all elements that match");
+            listCmdOpt.setOptionalArg(false);
+            listCmdOpt.setArgs(1);
+            listCmdOpt.setArgName("YAMLPattern");
+        Option delCmdOpt = new Option("d", DELETECMD, true, "Delete all elements that match");
+            delCmdOpt.setOptionalArg(false);
+            delCmdOpt.setArgs(1);
+            delCmdOpt.setArgName("YAMLPattern");
+        Option macroCmdOpt = new Option("m", MACROCMD, true, "run input YAML file thru a MACRO processor searching for ${ASUX::__} and replacing __ with values from Properties file");
             macroCmdOpt.setOptionalArg(false);
             macroCmdOpt.setArgs(1);
             macroCmdOpt.setArgName("propertiesFile");
         Option replCmdOpt = new Option("c", REPLACECMD, true, "change/replace all elements that match with json-string provided on cmdline");
             replCmdOpt.setOptionalArg(false);
-            replCmdOpt.setArgs(1);
-            replCmdOpt.setArgName("new");
+            replCmdOpt.setArgs(2);
+            replCmdOpt.setValueSeparator(' ');
+            replCmdOpt.setArgName("YAMLPattern> <newValue"); // Note: there's a trick in the parameter-string.. as setArgName() assumes a single 'word' and puts a '<' & '>' around that single-word.
+        Option batchCmdOpt = new Option("b", BATCHCMD, true, "run a batch of commands, which are listed in the <batchfile>");
+            macroCmdOpt.setOptionalArg(false);
+            macroCmdOpt.setArgs(1);
+            macroCmdOpt.setArgName("batchFile");
         grp.addOption(readCmdOpt);
-        grp.addOption(delCmdOpt);
-        grp.addOption(macroCmdOpt);
         grp.addOption(listCmdOpt);
+        grp.addOption(delCmdOpt);
         grp.addOption(replCmdOpt);
+        grp.addOption(macroCmdOpt);
+        grp.addOption(batchCmdOpt);
         grp.setRequired(true);
 
         options.addOptionGroup(grp);
@@ -122,13 +145,16 @@ public class CmdLineArgs {
         options.addOptionGroup(grp2);
 
         //----------------------------------
-        opt = new Option("p", YAMLPATH, true, "Path to YAML element");
-        opt.setRequired(true);
-        options.addOption(opt);
-
-        // opt = new Option("pr", PROPERTIES, true, "File containing properties for ${ASUX::key}");
-        // opt.setRequired(false);
+        // opt = new Option("p", YAMLPATH, true, "Path to YAML element");
+        // opt.setRequired(true);
         // options.addOption(opt);
+
+        opt = new Option("zd", DELIMITER, false, "whether period/dot comma pipe or other character is the delimiter to use within the YAMLPATHPATTERN");
+        opt.setRequired(false);
+        opt.setArgs(1);
+        opt.setOptionalArg(false);
+        opt.setArgName("delimcharacter");
+        options.addOption(opt);
 
         opt = new Option("i", INPUTFILE, true, "input file path");
         opt.setRequired(true);
@@ -146,24 +172,41 @@ public class CmdLineArgs {
             cmd = parser.parse(options, args);
 
             this.verbose = cmd.hasOption("verbose");
+            this.yamlPatternDelimiter = cmd.getOptionValue(DELIMITER);
+            if ( this.yamlPatternDelimiter == null || this.yamlPatternDelimiter.equals(".") )
+                this.yamlPatternDelimiter = YAMLPath.DEFAULTDELIMITER;
+
             this.isReadCmd = cmd.hasOption(READCMD);
             this.isListCmd = cmd.hasOption(LISTCMD);
             this.isDelCmd = cmd.hasOption(DELETECMD);
-            this.isMacroCmd = cmd.hasOption(MACROCMD);
             this.isReplaceCmd = cmd.hasOption(REPLACECMD);
+            this.isMacroCmd = cmd.hasOption(MACROCMD);
+            this.isBatchCmd = cmd.hasOption(BATCHCMD);
 
-            this.yamlPathStr = cmd.getOptionValue(YAMLPATH);
+            // this.yamlRegExpStr = cmd.getOptionValue(YAMLPATH);
             this.inputFilePath = cmd.getOptionValue(INPUTFILE);
-            // following two are optional arguments.
             this.outputFilePath = cmd.getOptionValue(OUTPUTFILE);
+
+            // following are defined to be optional arguments, but mandatory for a specific command (as you can see from the condition of the IF statements).
+            if (this.isReadCmd) this.yamlRegExpStr = cmd.getOptionValue(READCMD);
+            if (this.isListCmd) this.yamlRegExpStr = cmd.getOptionValue(LISTCMD);
+            if (this.isDelCmd) this.yamlRegExpStr = cmd.getOptionValue(DELETECMD);
+            final String[] searchArgs = cmd.getOptionValues(REPLACECMD);
+            // because we set .setArgs(2) above.. you can get the values for:- searchArgs[0] and searchArgs[1].
+            if (this.isReplaceCmd) this.yamlRegExpStr = searchArgs[0]; // 1st of the 2 arguments for REPLACE cmd.
+            if (this.isReplaceCmd) this.replaceFilePath = searchArgs[1];
+
             this.replaceFilePath = cmd.getOptionValue(REPLACECMD);
             this.propertiesFilePath = cmd.getOptionValue(MACROCMD);
+            this.batchFilePath = cmd.getOptionValue(BATCHCMD);
 
-//            this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE; // default behavior
-//            if ( cmd.hasOption( noQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.NONE;
-//            if ( cmd.hasOption( singleQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE;
-//            if ( cmd.hasOption( doubleQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.DOUBLEQUOTE;
-            //System.err.println("this.quoteType = "+this.quoteType.toString());
+            // this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE; // default behavior
+            // if ( cmd.hasOption( noQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.NONE;
+            // if ( cmd.hasOption( singleQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.SINGLEQUOTE;
+            // if ( cmd.hasOption( doubleQuoteOpt.getLongOpt()) ) this.quoteType = com.esotericsoftware.yamlbeans.YamlConfig.QuoteCharEnum.DOUBLEQUOTE;
+            // System.err.println("this.quoteType = "+this.quoteType.toString());
+
+            // System.err.println( CLASSNAME +": "+this.toString());
 
         } catch (ParseException e) {
             e.printStackTrace(System.err);
@@ -176,7 +219,13 @@ public class CmdLineArgs {
     /** For making it easy to have simple code generate debugging-output, added this toString() method to this class.
      */
     public String toString() {
-        return "verbose="+verbose+" read="+isReadCmd+" list="+isListCmd+"  delete="+isDelCmd+" macro="+isMacroCmd+" change="+isReplaceCmd+" yamlPathStr="+yamlPathStr+" inpfile="+inputFilePath+" outputfile="+outputFilePath+" replaceFile="+replaceFilePath;
+        return "verbose="+verbose+" yamlPatternDelimiter="+yamlPatternDelimiter+" yamlRegExpStr="+yamlRegExpStr
+        +" read="+isReadCmd+" list="+isListCmd+"  delete="+isDelCmd+" change="+isReplaceCmd
+        +" macro="+isMacroCmd+" batch="+isBatchCmd
+        +" inpfile="+inputFilePath+" outputfile="+outputFilePath+" replaceFile="+replaceFilePath
+        +" batchFilePath="+batchFilePath+" propertiesFilePath="+propertiesFilePath
+        ;
+        // yamlRegExpStr="+yamlRegExpStr+" 
     }
     
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
