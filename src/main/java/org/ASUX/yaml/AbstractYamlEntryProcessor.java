9dc6d817216dbbe9d39f88d56c01eb6b842cb13b
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-04-24 20:35:25 -0400

    keys are Strings now.  LinkedHashMap<String, Object> replaced plain java.util.Map.  Added SuppressWarnings for compiler

--- src/main/java/org/ASUX/yaml/AbstractYamlEntryProcessor.java
+++ src/main/java/org/ASUX/yaml/AbstractYamlEntryProcessor.java
@@ -32,7 +32,8 @@
 
 package org.ASUX.yaml;
 
-import java.util.Map;
+// import java.util.Map;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.ArrayList;
 
@@ -44,7 +45,7 @@ import static org.junit.Assert.*;
  *  <p>This org.ASUX.yaml GitHub.com project and the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com projects, would
  *  simply NOT be possible without the genius Java library <a href="https://github.com/EsotericSoftware/yamlbeans">"com.esotericsoftware.yamlbeans"</a>.</p>
  *  <p>This abstract class has 4 concrete sub-classes (representing YAML-COMMANDS to read/query, list, delete and replace).</p>
- *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com project.</p>
+ *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org-ASUX.github.io/wiki">org.ASUX Wiki</a> of the GitHub.com projects.</p>
  * @see org.ASUX.yaml.ReadYamlEntry
  * @see org.ASUX.yaml.ListYamlEntry
  * @see org.ASUX.yaml.DeleteYamlEntry
@@ -78,14 +79,14 @@ public abstract class AbstractYamlEntryProcessor {
      * <p>That full/end2end match will trigger the other function "onEnd2EndMatch()".</p>
      *
      * <p>Do NOT fuck with (a.k.a alter) the contents of any of the parameters passed.   Use the parameters ONLY in Read-only manner.  Got itchy fingers?  Then, Deepclone both the parameters.  YAMLPath class has a static member-function to make it easy to deepClone.</p>
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the YAML SUB-tree (Note: Sub-tree) of the YAML file, as pointed to by "_yamlPath" and "_key".
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the YAML SUB-tree (Note: Sub-tree) of the YAML file, as pointed to by "_yamlPath" and "_key".
      *  @param _yamlPath See the class YAMLPath @see org.ASUX.yaml.YAMLPath
      *  @param _key The value (typically a String) is what matched the _yamlPath.  Use it to get the "rhs" of the YAML element pointed to by _key
      *  @param _parentMap A Placeholder to be used in the future.  Right now it's = null
      *  @param _end2EndPaths for _yamlPathStr, this java.util.LinkedList shows the "stack of matches".   Example:  ["paths", "/pet", "get", "responses", "200"]
      *  @return The concrete sub-class can return false, to STOP any further progress on this partial match
      */
-    protected abstract boolean onPartialMatch(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths);
+    protected abstract boolean onPartialMatch(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths);
 
     //-------------------------------------
     /** <p>This function will be called when a full/end2end match of a YAML path-expression happens.</p>
@@ -96,14 +97,14 @@ public abstract class AbstractYamlEntryProcessor {
      *  Hence the choice of onEnd2EndMath() as the function name.</p>
      *
      * <p>Do NOT fuck with (a.k.a alter) the contents of any of the parameters passed.   Use the parameters ONLY in Read-only manner.  Got itchy fingers?  Then, Deepclone both the parameters.  YAMLPath class has a static member-function to make it easy to deepClone.</p>
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the "bottom-most" YAML SUB-tree (Note: Sub-tree) of the YAML file, as pointed to by "_yamlPath" and "_key".  This map could be potentially represent a simple YAML element like "name: petid"
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the "bottom-most" YAML SUB-tree (Note: Sub-tree) of the YAML file, as pointed to by "_yamlPath" and "_key".  This map could be potentially represent a simple YAML element like "name: petid"
      *  @param _yamlPath See the class YAMLPath @see org.ASUX.yaml.YAMLPath
      *  @param _key The value (typically a String) is what matched the _yamlPath.  For "YAML Query", the "rhs" of the YAML element pointed to by _key is what you're looking for.  For "YAML Delete" or "YAML Replace", you do Not care about the "rhs".. just use the _key to remove the entry/replace the "rhs".
      *  @param _parentMap A Placeholder to be used in the future.  Right now it's = null
      *  @param _end2EndPaths for _yamlPathStr, this java.util.LinkedList shows the "stack of matches".   Example:  ["paths", "/pet", "get", "responses", "200"]
      *  @return The concrete sub-class can return false, to STOP any further progress on this partial match
      */
-    protected abstract boolean onEnd2EndMatch(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths);
+    protected abstract boolean onEnd2EndMatch(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths);
 
     //-------------------------------------
     /** <p>This function will be called whenever the YAML path-expression fails to match.</p>
@@ -112,13 +113,13 @@ public abstract class AbstractYamlEntryProcessor {
      * <p>Do NOT fuck with (a.k.a alter) the contents of any of the parameters passed.   Use the parameters ONLY in Read-only manner.  Got itchy fingers?  Then, Deepclone both the parameters.  YAMLPath class has a static member-function to make it easy to deepClone.</p>
      * <p>Note: Unlike the other abstract methods of this Abstract class, this does NOT have a return-value.</p>
      *
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the YAML SUB-tree (Note: Sub-tree) of the YAML file (as pointed to by "_yamlPath" and "_key") - representing where PathPattern match failed.  This map could be potentially represent a simple YAML element like "name: petid"
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the YAML SUB-tree (Note: Sub-tree) of the YAML file (as pointed to by "_yamlPath" and "_key") - representing where PathPattern match failed.  This map could be potentially represent a simple YAML element like "name: petid"
      *  @param _yamlPath See the class YAMLPath @see org.ASUX.yaml.YAMLPath
      *  @param _key The value (typically a String) is what *FAILED* to match the _yamlPath.
      *  @param _parentMap A Placeholder to be used in the future.  Right now it's = null
      *  @param _end2EndPaths for _yamlPathStr, this java.util.LinkedList shows the "stack of matches".   Example:  ["paths", "/pet", "get", "responses", "200"]
      */
-    protected abstract void onMatchFail(final Map _map, final YAMLPath _yamlPath, final Object _key, final Map _parentMap, final LinkedList<String> _end2EndPaths);
+    protected abstract void onMatchFail(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final String _key, final LinkedHashMap<String, Object> _parentMap, final LinkedList<String> _end2EndPaths);
 
     //-------------------------------------
     /** <p>This function will be called when processing has ended.</p>
@@ -126,28 +127,31 @@ public abstract class AbstractYamlEntryProcessor {
      *
      *  <p>You can fuck with the contents of any of the parameters passed, to your heart's content.</p>
      *
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
      *  @param _yamlPath See the class YAMLPath @see org.ASUX.yaml.YAMLPath
      */
-    protected abstract void atEndOfInput(final Map _map, final YAMLPath _yamlPath);
+    protected abstract void atEndOfInput(final LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath);
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     /** <p>Internal Note: This is <b>NOT NOT NOT NOT NOT</b> ........ a RECURSIVE-FUNCTION.</p>
      *  <p>This is a simple way to invoke the real-recursive function {@link #recursiveSearch}.</p>
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
      *  @param _yamlPathStr Example: "<code>paths.*.*.responses.200</code>" - <b>ATTENTION: This is a human readable pattern, NOT a proper RegExp-pattern</b>
+     *  @param _delim pass in a value like '.'  '\t'   ','   .. such a character as a string-parameter (being flexible in case delimiters can be more than a single character)
      *  @return true = whether at least one match happened.
      *  @throws com.esotericsoftware.yamlbeans.YamlException - this is thrown by the library com.esotericsoftware.yamlbeans
      */
-    public boolean searchYamlForPattern(Map _map, String _yamlPathStr) throws com.esotericsoftware.yamlbeans.YamlException {
+    public boolean searchYamlForPattern(LinkedHashMap<String, Object> _map, String _yamlPathStr, final String _delim)
+                throws com.esotericsoftware.yamlbeans.YamlException
+    {
         final LinkedList<String> end2EndPaths = new LinkedList<>();
-        final YAMLPath yp = new YAMLPath( _yamlPathStr );
+        final YAMLPath yp = new YAMLPath( _yamlPathStr, _delim );
         final boolean retval = this.recursiveSearch( _map, yp, end2EndPaths );
         atEndOfInput( _map, yp );
         // What should be done if atEndOfInput returns false.. ?
         return retval;
-   }
+    }
 
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
@@ -155,14 +159,15 @@ public abstract class AbstractYamlEntryProcessor {
      *  <p><b>Don't tell me I did NOT warn you!</b>  Use the {@link searchYamlForPattern} function instead.</p>
      *  <p>This function returns true, if the invocation (or it's recursion) did find a match (partial or end2end).<br>
      *  For now, I'm Not using the return value ANYWHERE.   Either I will - or - will refactor the return as Void.</p>
-     *  @param _map This contains the java.utils.Map (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
+     *  @param _map This contains the java.utils.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans library) containing the entire Tree representing the YAML file.
      *  @param _yamlPath This is the {@link YAMLPath} class consstructed using example strings like "<code>paths.*.*.responses.200</code>" - <b>ATTENTION: This string is a human readable pattern, NOT a proper RegExp-pattern</b>
      *  @param _end2EndPaths for _yamlPathStr, this java.util.LinkedList shows the "stack of matches".   Example:  ["paths", "/pet", "get", "responses", "200"]
      *  @return true = whether at least one match happened.
      *  @throws com.esotericsoftware.yamlbeans.YamlException - this is thrown by the library com.esotericsoftware.yamlbeans
      */
-    public boolean recursiveSearch(Map _map, final YAMLPath _yamlPath, final LinkedList<String> _end2EndPaths )
-    throws com.esotericsoftware.yamlbeans.YamlException {
+    public boolean recursiveSearch(LinkedHashMap<String, Object> _map, final YAMLPath _yamlPath, final LinkedList<String> _end2EndPaths )
+                    throws com.esotericsoftware.yamlbeans.YamlException
+    {
         if ( _map == null || (! _yamlPath.isValid) ) return false;
         if ( ! _yamlPath.hasNext() ) return false; // YAML path has ended
 
@@ -172,7 +177,7 @@ public abstract class AbstractYamlEntryProcessor {
         boolean aMatchFound = false;
 
         //--------------------------
-        for (Object key : _map.keySet()) {
+        for (String key : _map.keySet()) {
 
             final Object rhs = _map.get(key);  // otherwise we'll inefficiently be doing map.get multiple times below.
             final String rhsStr = rhs.toString(); // to make verbose logging code simplified
@@ -187,7 +192,7 @@ public abstract class AbstractYamlEntryProcessor {
                 hasThisYamlLineMatched = true;
             } else {
                 final Pattern yamlPElemPatt = java.util.regex.Pattern.compile(yamlPathElemStr); // This should Not throw, per precautions in YAMLPath class
-                hasThisYamlLineLiterallyMatched = yamlPElemPatt.matcher(key.toString()).matches();
+                hasThisYamlLineLiterallyMatched = yamlPElemPatt.matcher(key).matches();
                 hasThisYamlLineMatched = hasThisYamlLineLiterallyMatched;
             }
             // One more check: If current YamlLine's key did NOT match, but is there a "**" for a "greedy-match"
@@ -199,7 +204,7 @@ public abstract class AbstractYamlEntryProcessor {
             if ( hasThisYamlLineMatched ) {
                 if ( this.verbose ) System.out.println(CLASSNAME + ": @# " + _yamlPath.index() +"\t"+ _yamlPath.getPrefix() +"\t"+ _yamlPath.get() +"\t"+ _yamlPath.getSuffix() + "\t matched("+ hasThisYamlLineLiterallyMatched+ ") '"+ key +"':\t"+ rhsStr.substring(0,rhsStr.length()>121?120:rhsStr.length()) +"\t\t of type '"+rhs.getClass().getName() +"'");
 
-                _end2EndPaths.add( key.toString() ); // _end2EndPaths keeps the breadcrumbs
+                _end2EndPaths.add( key ); // _end2EndPaths keeps the breadcrumbs
 
                 //------------------------------------------------------
                 assert (_yamlPath.hasNext()); // why on earth would this assertion fail - see checks @ top of function.
@@ -224,7 +229,7 @@ public abstract class AbstractYamlEntryProcessor {
 
                     if ( this.verbose ) System.out.println(CLASSNAME +": End2End Match#1 in YAML-file: "+ _yamlPath.getPrefix() +" "+ key  +":\t"+  rhsStr.substring(0,rhsStr.length()>121?120:rhsStr.length()) +"\t\t type '"+rhs.getClass().getName() +"'");
 
-                    continue; // outermost for-loop (Object key : _map.keySet())
+                    continue; // outermost for-loop (String key : _map.keySet())
                 }
                 //else .. continue below.
 
@@ -240,31 +245,41 @@ public abstract class AbstractYamlEntryProcessor {
                 //--------------------------------------------------------
                 // if we are here, we've only a PARTIAL match.
                 // So.. we need to keep recursing (specifically for Map & ArrayList YAML elements)
-                if ( rhs instanceof Map ) {
+                if ( rhs instanceof LinkedHashMap ) {
 
-                    aMatchFound = this.recursiveSearch( (Map)rhs, cloneOfYAMLPath, cloneOfE2EPaths); // recursion call
+                    @SuppressWarnings("unchecked")
+                    final LinkedHashMap<String, Object> rhs2 = (LinkedHashMap<String, Object>) rhs;
+                    aMatchFound = this.recursiveSearch( rhs2, cloneOfYAMLPath, cloneOfE2EPaths); // recursion call
 
                 } else if ( rhs instanceof java.util.ArrayList ) {
 
                     ArrayList arr = (ArrayList) rhs;
                     for ( Object o: arr ) {
                         // iterate over each element?  Need a new variant of this function - to match "index"
-                        if ( o instanceof Map ) { // Shouldn't this always be true - per YAML spec?
-                            aMatchFound = this.recursiveSearch( (Map)o, cloneOfYAMLPath, cloneOfE2EPaths); // recursion call
+                        if ( o instanceof LinkedHashMap ) { // Shouldn't this always be true - per YAML spec?
+
+                            @SuppressWarnings("unchecked")
+                            final LinkedHashMap<String, Object> o2 = (LinkedHashMap<String, Object>) o;
+                            aMatchFound = this.recursiveSearch( o2, cloneOfYAMLPath, cloneOfE2EPaths); // recursion call
+
                         } else if ( o instanceof java.lang.String ) {
                             // can't be a match, as it's Not even in the format   "rhs: lhs"
                             onMatchFail(_map, _yamlPath, key, null, _end2EndPaths); // location #1 for failure-2-match
+
                         } else {
                             System.err.println(CLASSNAME +": incomplete code: failure w Array-type '"+ o.getClass().getName() +"'");
                             onMatchFail(_map, _yamlPath, key, null, _end2EndPaths); // location #2 for failure-2-match
+
                         } // if-Else   o instanceof Map - (WITHIN FOR-LOOP)
                     } // for Object o: arr
 
                 } else if ( rhs instanceof java.lang.String ) {
+
                     if ( _yamlPath.hasNext() ) { // then it's Not an end2end match
                         _end2EndPaths.removeLast();
                         continue;
                     } else {
+
                         // yeah! We found a full end2end match!  Also, No more recursion is feasible.
                         // let sub-classes determine what to do here
                         final boolean callbkRet5 = onEnd2EndMatch(_map, _yamlPath, key, null, cloneOfE2EPaths); // location #2 for end2end match
@@ -275,6 +290,7 @@ public abstract class AbstractYamlEntryProcessor {
                     }
 
                 } else {
+
                     System.err.println(CLASSNAME +": incomplete code: Unable to handle rhs of type '"+ rhs.getClass().getName() +"'");
                     onMatchFail(_map, _yamlPath, key, null, _end2EndPaths); // location #3 for failure-2-match
 
