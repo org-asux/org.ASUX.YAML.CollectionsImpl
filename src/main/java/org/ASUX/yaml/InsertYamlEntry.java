6109e46160ee10fd2779d45cd35fef5e339b8f91
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-05-14 18:19:03 -0400

    impact of new org.ASUX.common.jar..  Removing all traces of com.esotericsoftware

--- src/main/java/org/ASUX/yaml/InsertYamlEntry.java
+++ src/main/java/org/ASUX/yaml/InsertYamlEntry.java
@@ -32,6 +32,9 @@
 
 package org.ASUX.yaml;
 
+import org.ASUX.common.Tuple;
+import org.ASUX.common.Output;
+
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -39,8 +42,7 @@ import java.util.LinkedHashMap;
 /** <p>This concrete class is minimalistic because I am re-using code to query/traverse a YAML file.   See it's parent-class {@link org.ASUX.yaml.AbstractYamlEntryProcessor}.</p>
  *  <p>This concrete class is part of a set of 4 concrete sub-classes (representing YAML-COMMANDS to read/query, list, delete and replace ).</p>
  *  <p>This class contains implementation for 4 "callbacks" - </p><ol><li> whenever there is partial match - on the way to a complete(a.k.a. end2end match) </li><li> whenever a full match is found </li><li> a match failed (which implies, invariably, to keep searching till end of YAML file - but.. is a useful callback if you are using a "negative" pattern to search for YAML elements) </li><li> done processing entire YAML file</li></ol>
- *  <p>This org.ASUX.yaml GitHub.com project and the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com projects, would
- *  simply NOT be possible without the genius Java library <a href="https://github.com/EsotericSoftware/yamlbeans">"com.esotericsoftware.yamlbeans"</a>.</p>
+ *  <p>This org.ASUX.yaml GitHub.com project and the <a href="https://github.com/org-asux/org.ASUX.cmdline">org.ASUX.cmdline</a> GitHub.com projects.</p>
  *  <p>See full details of how to use this, in {@link org.ASUX.yaml.Cmd} as well as the <a href="https://github.com/org-asux/org-ASUX.github.io/wiki">org.ASUX Wiki</a> of the GitHub.com projects.</p>
  * @see org.ASUX.yaml.AbstractYamlEntryProcessor
  */
@@ -49,9 +51,9 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
     public static final String CLASSNAME = "org.ASUX.yaml.InsertYamlEntry";
 
     // Note: We need to remove the "old" - exactly as DeleteYamlEntry.java does.  Then we insert new value.
-    protected final ArrayList< Tools.Tuple< String, LinkedHashMap<String, Object> > >
+    protected final ArrayList< Tuple< String, LinkedHashMap<String, Object> > >
                     existingPathsForInsertion = new ArrayList<>();
-    protected final ArrayList< Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > >
+    protected final ArrayList< Tuple< YAMLPath, LinkedHashMap<String, Object> > >
                     newPaths2bCreated = new ArrayList<>();
     protected Object newData2bInserted = "";
 
@@ -61,7 +63,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
 
     /** The only Constructor.
      *  @param _verbose Whether you want deluge of debug-output onto System.out
-     *  @param _nob this can be either a java.lang.String or a java.util.LinkedHashMap&lt;String, Object&gt; (created by com.esotericsoftware.yamlbeans)
+     *  @param _nob this can be either a java.lang.String or a java.util.LinkedHashMap&lt;String, Object&gt; (created by YAMLReader classes from various libraries)
      *  @param _showStats Whether you want a final summary onto console / System.out
      *  @throws java.lang.Exception - if the _nob parameter is not as per above Spec
      */
@@ -70,12 +72,14 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
         if ( _nob == null )
             throw new Exception( CLASSNAME + ": constructor(): _nob parameter is Null");
 
-        final Tools tools = new Tools(this.verbose);
-        final Tools.OutputObjectTypes typ = tools.getOutputObjectType( _nob );
-        Object o = tools.getTheActualObject( _nob ); // perhaps the object is already wrapped (via a prior invocation of Tools.wrapAnObject_intoLinkedHashMap() )
+        final Output output = new Output(this.verbose);
+        final Tools tools = new Tools( this.verbose );
+        final Output.OutputType typ = output.getWrappedObjectType( _nob );
+
+        Object o = output.getTheActualObject( _nob ); // perhaps the object is already wrapped (via a prior invocation of output.wrapAnObject_intoLinkedHashMap() )
         if (this.verbose) System.out.println( CLASSNAME +": constructort(): provided ["+ _nob.toString() +"].  I assume the actual object of type=["+ typ.toString() +"]=["+ o.toString() +"]" );
 
-        // for the following SWITCH-statement, keep an eye on Tools.OutputObjectTypes
+        // for the following SWITCH-statement, keep an eye on Output.OutputType
         switch( typ ) {
             case Type_ArrayList:
             case Type_LinkedList:
@@ -89,7 +93,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
                 // Convert Strings into YAML/JSON compatible LinkedHashMap .. incl. converting Key=Value  --> Key: Value
                 LinkedHashMap<String, Object> map = null; // let's determine if o is to be treated as a LinkedHashMap.. because user provided a JSON or YAML inline to the command line.
 
-                // IF-and-ONLY-IF _nob is a simple-scalar-String, then this call will wrap the String by calling Tools.wrapAnObject_intoLinkedHashMap()
+                // IF-and-ONLY-IF _nob is a simple-scalar-String, then this call will wrap the String by calling output.wrapAnObject_intoLinkedHashMap()
                 try{
                     // more than likely, we're likely to see a JSON as a string - inline - within the command (or in a batch-file line)
                     // and less likely to see a YAML string inline
@@ -107,7 +111,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
                 } // outer-try-catch
 
                 this.newData2bInserted = (map != null)? map : o;
-                // if ( s.equals( tools.getTheActualObject(map).toString() ) )
+                // if ( s.equals( output.getTheActualObject(map).toString() ) )
                 //     o = s; // IF-and-ONLY-IF _nob is a simple-scalar-String, then use it as-is.
                 break;
             case Type_KVPair:
@@ -145,7 +149,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
             _end2EndPaths.forEach( s -> System.out.print(s+", ") );
             System.out.println("");
         }
-        this.existingPathsForInsertion.add( new Tools.Tuple< String, LinkedHashMap<String, Object> >(_key, _map) );
+        this.existingPathsForInsertion.add( new Tuple< String, LinkedHashMap<String, Object> >(_key, _map) );
         if ( this.verbose ) System.out.println( CLASSNAME +": onE2EMatch(): count="+this.existingPathsForInsertion.size());
         return true;
     }
@@ -161,7 +165,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
         // Also, we'll have SO MANY DUPLICATES!
         if ( _yamlPath == null || _map == null )
             return;
-        final Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > tuple = new Tools.Tuple<>( YAMLPath.deepClone(_yamlPath), _map );
+        final Tuple< YAMLPath, LinkedHashMap<String, Object> > tuple = new Tuple<>( YAMLPath.deepClone(_yamlPath), _map );
         if ( this.verbose ) System.out.println( CLASSNAME +": onMatchFail():>>>>>>>>>>>> _yamlPath="+ _yamlPath.toString() );
         if ( this.verbose ) System.out.println( CLASSNAME +": onMatchFail():>>>>>>>>>>>> tuple="+ tuple.key );
         this.newPaths2bCreated.add( tuple );
@@ -182,13 +186,13 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
     {
         if ( YAMLPath.ROOTLEVEL.equals( _yamlPath.getRaw() ) ) // '/' is the entire YAML-Path pattern
         {
-            final Tools tools = new Tools( this.verbose );
-            final Tools.OutputObjectTypes typ = tools.getOutputObjectType( this.newData2bInserted );
+            final Output output = new Output( this.verbose );
+            final Output.OutputType typ = output.getWrappedObjectType( this.newData2bInserted );
     
             switch(typ) {
                 case Type_KVPair:  // singular;  No 's' character @ end.  This is Not KVPairs
                     @SuppressWarnings("unchecked")
-                    final Tools.Tuple< String,String > kvpair = ( Tools.Tuple< String,String > ) this.newData2bInserted;
+                    final Tuple< String,String > kvpair = ( Tuple< String,String > ) this.newData2bInserted;
                     _map.put( kvpair.key, kvpair.val );
                     break;
                 case Type_KVPairs:  // PLURAL;  Note the 's' character @ end.  This is Not KVPair (singular)
@@ -211,15 +215,15 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
         }
 
         // first loop goes over Paths that already exist, in the sense the leaf-element exists, and we'll add a new Child element to that.
-        for ( Tools.Tuple< String, LinkedHashMap<String, Object> > tpl: this.existingPathsForInsertion ) {
+        for ( Tuple< String, LinkedHashMap<String, Object> > tpl: this.existingPathsForInsertion ) {
             final String rhsStr = tpl.val.toString();
             if ( this.verbose ) System.out.println( CLASSNAME +": atEndOfInput(): key=["+ tpl.key +"], while map-in-context="+ rhsStr.substring(0,rhsStr.length()>121?120:rhsStr.length()) );
             // tpl.val.remove(tpl.key);
 
             // Now put in a new entry - with the replacement data!
-            tpl.val.put( tpl.key, Tools.deepClone( this.newData2bInserted ) );
+            tpl.val.put( tpl.key, org.ASUX.common.Utils.deepClone( this.newData2bInserted ) );
             if ( this.verbose ) System.out.println( CLASSNAME +": atEndOfInput(): key=["+ tpl.key +"], it's new value="+ this.newData2bInserted.toString() );
-            // If there are multiple matches.. then without deepclone, the EsotericSoftware
+            // If there are multiple matches.. then without deepclone, the YAML implementation libraries (like Eso teric Soft ware)
             // library, will use "&1" to define your 1st copy (in output) and put "*1" in
             // all other locations this replacement text WAS SUPPOSED have been :-(
         }
@@ -228,22 +232,22 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
         // IMPORTANT: See the comments inside onMatchFail
         // We need to 'cull' the entries within this.newPaths2bCreated
         int longestDepth = -1;
-        for ( Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : this.newPaths2bCreated ) {
+        for ( Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : this.newPaths2bCreated ) {
             final YAMLPath yp = tpl.key;
             if ( yp.index()> longestDepth ) longestDepth = yp.index();
         }
 
         if ( this.verbose ) System.out.println( CLASSNAME +": atEndOfInput(): longestDepth ="+ longestDepth +"]" );
-        final ArrayList< Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > >
+        final ArrayList< Tuple< YAMLPath, LinkedHashMap<String, Object> > >
                     deepestNewPaths2bCreated = new ArrayList<>();
 
         outerloop:
-        for ( Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : this.newPaths2bCreated ) {
+        for ( Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : this.newPaths2bCreated ) {
             final YAMLPath ypNew = tpl.key;
             if ( this.verbose ) System.out.println( CLASSNAME +": atEndOfInput(): ypNew ="+ ypNew +"]" );
             if ( ypNew.index() >= longestDepth ) {
                 // let's check .. have we added it already?
-                for ( Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl2 : deepestNewPaths2bCreated ) {
+                for ( Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl2 : deepestNewPaths2bCreated ) {
                     final YAMLPath ypE = tpl2.key;
                     if ( YAMLPath.areEquivalent( ypNew, ypE ) )
                         continue outerloop;
@@ -259,7 +263,7 @@ public class InsertYamlEntry extends AbstractYamlEntryProcessor {
         // This 2nd loop is going to deal with WITH MISSING 'paths' to the missing leaf-element.
         // similar to how 'mkdir -p' works on Linux.
         if ( this.verbose ) System.out.println( CLASSNAME +": atEndOfInput(): deepestNewPaths2bCreated.size()="+ deepestNewPaths2bCreated.size() +"]" );
-        for ( Tools.Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : deepestNewPaths2bCreated ) {
+        for ( Tuple< YAMLPath, LinkedHashMap<String, Object> > tpl : deepestNewPaths2bCreated ) {
             final YAMLPath yp = tpl.key;
             final LinkedHashMap<String, Object> lowestmap = tpl.val;
             final String prefix = yp.getPrefix();
