3a0849a61624be5f62e07f07bfde1b62032375f1
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-05-22 22:33:50 -0400

    now that there is a Macros.java in YAML parent-project, this file got whacked down in size

--- src/main/java/org/ASUX/yaml/CollectionsImpl/MacroYamlProcessor.java
+++ src/main/java/org/ASUX/yaml/CollectionsImpl/MacroYamlProcessor.java
@@ -94,7 +94,7 @@ public class MacroYamlProcessor {
      *  @param _outpMap Pass in a new 'empty' java.utils.LinkedHashMap&lt;String, Object&gt;().  THis is what this function *RETURNS* after Macros are evalated within _inpMap
      *  @param _props can be null, otherwise an instance of {@link java.util.Properties}
      *  @return true = whether at least one match of ${ASUX::} happened.
-	 *  @throws MacroYamlProcessor.MacroException - thrown if any attempt to evaluate MACROs fails within evaluateMacros() functions
+	 *  @throws MacroYamlProcessor.MacroException - thrown if any attempt to evaluate MACROs fails within org.ASUX.yaml.Macros.eval() functions
 	 *  @throws Exception - forany other run time error (especially involving YAML issues)
      */
     public boolean recursiveSearch(
@@ -111,7 +111,7 @@ public class MacroYamlProcessor {
         //--------------------------
         for (Object keyAsIs : _inpMap.keySet()) {
 
-            final String key = evaluateMacros( keyAsIs.toString(), _props );
+            final String key = org.ASUX.yaml.Macros.eval( keyAsIs.toString(), _props );
             assert( key != null );
 
             // Note: the lookup within _inpMap .. uses keyAsIs.  Not key.
@@ -151,7 +151,7 @@ public class MacroYamlProcessor {
 						newarr.add( newMap2 );
 					} else if ( o instanceof java.lang.String ) {
 						// by o.toString(), I'm cloning the String object.. .. so both _inpMap and _outpMap do NOT share the same String object
-						newarr.add ( evaluateMacros( o.toString(), _props ) );
+						newarr.add ( org.ASUX.yaml.Macros.eval( o.toString(), _props ) );
 					} else {
 						System.err.println( CLASSNAME +": recursiveSearch(): incomplete code #1: failure w Array-type '"+ o.getClass().getName() +"'");
 						System.exit(92); // This is a serious failure. Shouldn't be happening.
@@ -164,7 +164,7 @@ public class MacroYamlProcessor {
 			} else if ( rhsObj instanceof java.lang.String ) {
 				// by rhsObj.toString(), I'm cloning the String object.. .. so both _inpMap and _outpMap do NOT share the same String object
 				final String asis = rhsObj.toString();
-				final String news = evaluateMacros( asis, _props);
+				final String news = org.ASUX.yaml.Macros.eval( asis, _props);
 				if (   !    asis.equals(news) ) this.changesMade ++;
 				_outpMap.put( key, news );
 				// Well: If the key != keyAsIs .. then .. the resulting entry in YAML outputfile is something like '"key"' (that is, a single+double-quote problem)
@@ -182,94 +182,5 @@ public class MacroYamlProcessor {
 
         return bChangesMade;
     }
-
-	//=============================================================================
-	//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-	//=============================================================================
-
-	//------------------------------------------------------------------
-	public static final String pattStr = "[$]\\{ASUX::([^}]+)\\}";
-
-	//------------------------------------------------------------------
-	/**
-	 * <p>Pass in a string as 1st parameter and a properties file as 2nd parameter.</p>
-	 * <p>All instances of ${ASUX::___} are replaced with values for ___ (within Properties instance).</p>
-	 * Any other expressions like ${XYZABC} are LEFT UNTOUCHED, as it does NOT have the ASUX:: prefixc.
-	 * @param _s the string which CAN (not required to) contain macro expressions like ${ASUX::___}
-	 * @param _props a java.util.Properties object (null will mean function returns immediately)
-	 * @return the original string as-is (if no macros were detected).. or the altered version
-	 * @throws MacroYamlProcessor.MacroException if ANY failure in evaluating the macro on the input _s
-	 */
-	public static String evaluateMacros( final String _s, final Properties _props) throws MacroYamlProcessor.MacroException {
-		if (_s==null) return null;
-		if (_props==null) return _s;
-
-		try {
-
-			Pattern pattern = Pattern.compile( MacroYamlProcessor.pattStr );
-			Matcher matcher = pattern.matcher( _s );
-
-			boolean found = false;
-			String retstr = "";
-			int prevIndex = 0;
-
-			while (matcher.find()) {
-				found = true;
-				// System.out.println("I found the text "+matcher.group()+" starting at index "+  matcher.start()+" and ending at index "+matcher.end());    
-				retstr += _s.substring( prevIndex, matcher.start() );
-				final String macroStr = matcher.group(1);
-				final String v = _props.getProperty( macroStr ); // lookup value for ${ASUX::__} and add it to retStr
-
-				if ( v == null )
-					retstr += "${ASUX::"+ macroStr +"}"; // we are NOT going to replace      ${ASUX::unknownVARIABLE}      with        null
-				else
-					retstr += v;
-				prevIndex = matcher.end();
-				// System.out.println( CLASSNAME + ": Matched: "+matcher.group(1)+" to ["+ v +"]@ index "+ matcher.start()+" and ending @ "+matcher.end());
-			}
-
-			if(found){
-				if ( prevIndex < _s.length() ) {
-					// whatever is LEFT ***AFTER*** the last match.. we can't forget about that!
-					retstr += _s.substring( prevIndex );
-				}
-				// System.out.println( "Properties LOOKUP found: for ["+ retstr +"]");
-				return retstr;
-			} else {
-				// System.out.println("No match found.");
-				return _s;
-			}
-
-		} catch (PatternSyntaxException e) {
-			e.printStackTrace(System.err);
-			final String s = "PatternSyntaxException when checking if '" + _s + "' matches pattern " + MacroYamlProcessor.pattStr;
-			System.err.println(CLASSNAME + ": evaluateMacros(): Unexpected Internal ERROR: " + s );
-			throw new MacroException( s );
-		}
-
-		// return _s; // program control should never get here.
-	} // function
-
-	//------------------------------------------------------------------
-	/**
-	 * This is a variant of evaluateMacros(), to support Batch-Cmd mode, where BatchFile can load MULTIPLE property-files
-	 * @param _s the string which CAN (not required to) contain macro expressions like ${ASUX::___}
-	 * @param _propsSet a java.util.Properties object (null will mean function returns immediately)
-	 * @return the original string as-is (if no macros were detected).. or the altered version
-	 * @throws MacroYamlProcessor.MacroException - thrown if any attempt to evaluate MACROs fails within evaluateMacros() functions
-	 */
-	public static String evaluateMacros( final String _s, final LinkedHashMap<String,Properties> _propsSet )
-						throws MacroYamlProcessor.MacroException
-	{
-		String retStr = _s;
-		for( String key: _propsSet.keySet() ) {
-			final Properties p = _propsSet.get(key);
-			final String newstr = evaluateMacros( retStr, p);
-			if ( newstr != null ) // this can happen if the _s is just the MACRO only.  Example:    ${ASUX::variable}
-				retStr = newstr;
-			// We can have multiple variables, for which values can be in different properties files
-		}
-		return retStr;
-	}
 
 }
